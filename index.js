3b2e29e30e49aba8bd3b60e1606f9237a32385da
Kurtis Leach <danceflurry4366@gmail.com>
2019-03-14 18:11:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,6 @@ function updateObjectWithKeyAndValue (object, key, value) {
 function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
     return Object.assign(object, { [key]: value })
 }
+function deleteFromObjectByKey (object, key){
+  delete object.key
+}
